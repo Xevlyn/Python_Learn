7ab2e5ad1cd5b796ad8feb286c972df880057892
Xevlyn <ewanxev237@aol.com>
2022-10-12 12:50:10 +0100

    Day 1 learning

--- Day 1/Datatypes_Number.py
+++ Day 1/Datatypes_Number.py
@@ -2,7 +2,7 @@
 
 #Integer is a number example 1,2,3,4,5
 #Float is a floating point number or a number which is a decimal example 0.5,0.2
-#Float takes more space in memory as compared to the interger 
+#Float takes more space in memory as compared to the integer 
 #Can be used in mathematical situations 
 print(type(2 + 4)) #type tells us what data type the output is here
 print(type(2 * 4))
@@ -10,5 +10,5 @@ print(type(2 / 4))
 print(type(2 - 4))
 
 print(2 ** 2) #to the power of
-print(10 // 7) #returns nad interger rounded down
-print(2 % 4) #modulo is used to represent whats the remainder of the divivsion
+print(10 // 7) #returns nad integer rounded down
+print(2 % 4) #modulo is used to represent whats the remainder of the division
