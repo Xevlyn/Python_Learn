a9b0bb8a884f8ca58df0fdfd51011f3576e54e2a
Xevlyn <ewanxev237@aol.com>
2022-10-15 11:05:51 +0100

    Day 1

--- Day 1/Operator Precedence.py
+++ Day 1/Operator Precedence.py
@@ -1,4 +1,4 @@
-# Operator precedence is the idea that in a given example, diffrent operators have diffrent precedence over other operators
+# Operator precedence is the idea that in a given example, different operators have different precedence over other operators
 # Example 2 * 1 + 3 where in this example, 2 and 1 are multiplied first before being added to 3 
 
 print(20 - 3 * 4)
--- Day 1/Optional Bin() and Complex.py
+++ Day 1/Optional Bin() and Complex.py
@@ -2,5 +2,11 @@
 # used mostly in complex math functions and programs that use it
 # the function bin returns the binary representation of the integers
 
+#Basically these are number conversions
+#ob at the beginning  of it is not part of the actual conversions
+#To remove the ob use format (num, 'b')
+
 print(bin(5)) #lookup the documentation
 print(int('0b101', 2)) #Converts the binary number into an integer
+
+print(4 / 2 + 1 * 4 - 4)
